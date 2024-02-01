61626c16fa56a060ed710313eadc1cec458be86a
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-02-01 18:48:25 +0900

    Modified to use OCA plugins for CN

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
-provider "oci" {
-  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaa3mb7wrcy2ls3u3jsy2soq5ck3lc3q4mczitpdaymbuazc5tkguca"
-  user_ocid        = "ocid1.user.oc1..aaaaaaaajakyv7vrxpn2qhdexpqgofo2ere4kxpdgbldwhdvyrrdqgokaygq"
-  private_key_path = "/home/opc/.oci/oci_api_key_orasejapan.pem"
-  fingerprint      = "62:b0:07:ab:ea:6b:2a:71:50:45:46:c9:26:50:9a:24"
-  region           = "ap-tokyo-1"
-  auth                = "SecurityToken"
-  config_file_profile = "OSJ_token"
-}
+#provider "oci" {
+#  tenancy_ocid     = "xxxx"
+#  user_ocid        = "xxxx"
+#  private_key_path = "xxxx"
+#  fingerprint      = "xxxx"
+#  region           = "ap-osaka-1"
+#  region           = "ap-tokyo-1"
+#}
